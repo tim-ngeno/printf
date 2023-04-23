d8c2532d8004d1d70615360aa9a5a83896418469
Mukui012 <spencermukui@gmail.com>
2023-04-23 05:35:02 -0700

    add the header files

--- main.h
+++ main.h
@@ -11,8 +11,8 @@
 /*memory allocation*/
 void *_calloc(unsigned int nmemb, unsigned int size);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void write_buffer(bits_c *bin);
-void puts_buffer(bits_c *bin, char *str);
+void write_buffer(unsigned int bits_c *bin);
+void puts_buffer(unsigned int bits_c *bin, char *str);
 
 int _printf(const char *format, ...);
 
