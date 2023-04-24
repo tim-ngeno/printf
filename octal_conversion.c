5da25555a2ae3f8ec7d2acb9d40f0efa58b9ac55
Mukui012 <spencermukui@gmail.com>
2023-04-24 13:43:44 -0700

    add octal conversion

---
+++ octal_conversion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_octal - converts number to octal
+ *
+ * @n: the input integer
+ *
+ * Return: converted integer
+ */
+int print_octal(unsigned int n)
+{
+        if (n > 7)
+        {
+                _putchar(n / 8 + '0');
+                _putchar(n % 8 + '0');
+
+        }
+        else
+        {
+                _putchar(n + '0');
+        }
+
+        return (0);
+}
