b4d67cbbae48a94981a17ca54126d265ff7e6349
Mukui012 <spencermukui@gmail.com>
2023-04-24 00:44:21 -0700

    modify printf function

--- _printf.c
+++ _printf.c
@@ -1,75 +1,47 @@
 #include "main.h"
-#include <stdarg.h>
-#include <stddef.h>
 
 /**
- * print_string - print strings with the %s specifier
- *
- * @s: the input string
- *
- * Return: On success 0
- */
-int print_string(char *s)
-{
-	int i = 0;
-	int ret_value = 0;
-
-	if (s == NULL)
-		return (-1);
-
-	while (s[i] != '\0')
-	{
-		_putchar(s[i]);
-		i++;
-		ret_value += 1;
-	}
-
-	return (ret_value);
-}
-
-
-/**
- * _printf - print arguments passed to the function
- *
+ * _printf - print arguments passed to function
  * @format: pointer to argument specifier
- *
  * Return: 0 on success
  */
+
 int _printf(const char *format, ...)
 {
-	int i;
-	int j = 0; /*return value of _printf() */
-	int s_val; /* return value of print_string() */
-	va_list params;
+	va_list args_list;
+	stock_t *stk;
+	void (*temp_func)(stock_t *);
 
-	va_start(params, format);
+	if (!format)
+		return (-1);
+	va_start(args_list, format);
+	stk = build_stock(&args_list, format);
 
-	for (i = 0; format[i] != '\0'; i++)
+	while (stk && format[stk->i] && !stk->error)
 	{
-		if (format[i] != '%')
+		stk->c0 = format[stk->i];
+		if (stk->c0 != '%')
+			write_buffer(stk);
+		else
 		{
-			_putchar(format[i]);
+			parse_specifiers(stk);
+			temp_func = match_specifier(stk);
+			if (temp_func)
+				temp_func(stk);
+			else if (stk->c1)
+			{
+				if (stk->flag)
+					stk->flag = 0;
+				write_buffer(stk);
+			}
+			else
+			{
+				if (stk->space)
+					stk->buffer[--(stk->buf_index)] = '\0';
+				stk->error = 1;
+			}
 		}
-		else if (format[i + 1] == 'c')
-		{
-			_putchar(va_arg(params, int));
-			i++;
-		}
-		else if (format[i + 1] == '%')
-		{
-			_putchar('%');
-			i++;
-		}
-		else if (format[i + 1] == 's')
-		{
-			s_val = print_string(va_arg(params, char *));
-			i++;
-			j += (s_val - 1);
-		}
-
-		j += 1;
+		stk->i++;
 	}
-
-	va_end(params);
-	return (j);
+	return (end_func(stk));
 }
